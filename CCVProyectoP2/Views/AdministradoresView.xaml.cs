415883d1ba883adb2358536628e35eee14cc36ab
Kpiiale <yuliana.capito@udla.edu.ec>
2024-11-25 20:08:00 -0500

    Creación de carpeta de Views

---
+++ CCVProyectoP2/Views/AdministradoresView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CCVProyectoP2.Views;
+
+public partial class AdministradoresView : ContentPage
+{
+	public AdministradoresView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CCVProyectoP2/Views/EstudiantesView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CCVProyectoP2.Views;
+
+public partial class EstudiantesView : ContentPage
+{
+	public EstudiantesView()
+	{
+		InitializeComponent();
+	}
+}
