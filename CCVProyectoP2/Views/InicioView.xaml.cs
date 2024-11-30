a6d3c231bce676ffe06f7d39c0bd487c21f55d12
Kpiiale <yuliana.capito@udla.edu.ec>
2024-11-30 13:52:51 -0500

    Creación de Flyout y dos páginas: InicioView y SoporteView

---
+++ CCVProyectoP2/Views/InicioView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CCVProyectoP2.Views;
+
+public partial class InicioView : ContentPage
+{
+	public InicioView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CCVProyectoP2/Views/SoporteView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CCVProyectoP2.Views;
+
+public partial class SoporteView : ContentPage
+{
+	public SoporteView()
+	{
+		InitializeComponent();
+	}
+}
